7793f8b13e51fdfac0e4677922b099befc4019d9
Den611 <d.gerasim4uk@gmail.com>
2026-02-09 14:43:20 +0200

    Update web app URLs in config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "8578542989:AAFsaR62n5S3Wkh9RW4Ljdh98HuNLQd96OQ"
-WEB_APP_URL = "https://animated-torrone-883109.netlify.app/"
-WEB_APP_URL1 = "https://animated-torrone-883109.netlify.app/tree_earth"
+WEB_APP_URL = "https://rocket-lab.onrender.com"
+WEB_APP_URL1 = "https://rocket-lab.onrender.com/tree_Earth.html"
